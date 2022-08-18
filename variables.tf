136a53f47b7ca03c120ee08177ac813617525893
Frey Khademi <49865527+fkhademi@users.noreply.github.com>
2022-08-18 22:05:09 +0200

    Update variables.tf
    added user-data

--- variables.tf
+++ variables.tf
@@ -2,15 +2,19 @@
 variable "name" {
   description = "Name of the instance"
 }
+
 variable "region" {
   description = "Region to deploy the instance"
 }
+
 variable "vpc" {
   description = "VPC Name to deploy the instance in"
 }
+
 variable "subnet" {
   description = "Subnet Name to deploy the instance in"
 }
+
 variable "ssh_key" {
   description = "Public SSH key for the instance"
 }
@@ -20,6 +24,7 @@ variable "instance_size" {
   description = "Compute instance size"
   default     = "f1-micro"
 }
+
 variable "zone" {
   description = "Availability Zome for the instance"
   default     = "b"
@@ -28,4 +33,10 @@ variable "zone" {
 variable "public_ip" {
   description = "Set to true to assign a public IP to the instance"
   default     = false
+}
+
+variable "cloud_init_data" {
+  description = "User-data config"
+  default     = ""
+  type        = string
 }
